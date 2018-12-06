308aea78b556431fbf5b72dff3fc505a6c1ff829
Vincent Chan <vchan313@gmail.com>
2018-12-05 19:10:37 -0500

    added some cosmetics to one function

--- app/user.rb
+++ app/user.rb
@@ -20,10 +20,18 @@ class User < ActiveRecord::Base
 def save_property(address)
 property = Property.find_by(address: address) 
 Save.find_or_create_by(user_id: self.id, property_id: property.id)
+
+puts "Your Property has been saved!"
 end
 
 def all_my_saves
-Save.all.where(user_id: self.id)
+ puts "Here are you saved items:" 
+saved_properties = Save.all.where(user_id: self.id)
+saved_properties.each_with_index.map do |app,index|
+
+
+puts "#{index+1}. Address: #{Property.find(app.property_id).address}"
+  end
 end
 
 end
